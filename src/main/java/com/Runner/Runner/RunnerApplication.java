cbce53752725df66897ba127940b710f6ccd99fa
Tapio Nevalainen <ecoor1@gmail.com>
2018-09-23 04:32:10 +0300

    Hello world testi

--- src/main/java/com/Runner/Runner/RunnerApplication.java
+++ src/main/java/com/Runner/Runner/RunnerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RunnerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RunnerApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(RunnerApplication.class, args);
+    }
 }
