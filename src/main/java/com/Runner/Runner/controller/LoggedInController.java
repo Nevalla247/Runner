63d95e25f700ac6fe8e874ada50ce78f9d61d581
Tapio Nevalainen <ecoor1@gmail.com>
2018-09-30 15:27:50 +0300

    Lisättiin templaatit proofilille, juoksutiedoille ja ryhmille

---
+++ src/main/java/com/Runner/Runner/controller/LoggedInController.java
@@ -0,0 +1,93 @@
+
+package com.Runner.Runner.controller;
+
+import com.Runner.Runner.domain.Account;
+import com.Runner.Runner.domain.User;
+import com.Runner.Runner.repository.AccountRepository;
+import com.Runner.Runner.repository.UserRepository;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Tee
+ */
+
+@Controller
+public class LoggedInController {
+    
+    @Autowired
+    private AccountRepository accRepo;
+    
+    
+    @Autowired
+    private UserRepository usrRepo;
+    
+    @GetMapping("/profile")
+    public String profile(Model model){
+        
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        
+        Account account = accRepo.findByUsername(username);
+        
+        User usr = usrRepo.findByAccount(account);
+            
+            
+        model.addAttribute("name",usr.getFirst_name());
+        
+        Date now = new Date();
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("EEEE, MMMM d, yyyy");
+        
+        model.addAttribute("date", dateFormatter.format(now));
+        
+        return "profile";
+    }
+    
+    @GetMapping("/runs")
+    public String runs(Model model){
+        
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        
+        Account account = accRepo.findByUsername(username);
+        
+        User usr = usrRepo.findByAccount(account);
+            
+            
+        model.addAttribute("name",usr.getFirst_name());
+        
+        Date now = new Date();
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("EEEE, MMMM d, yyyy");
+        
+        model.addAttribute("date", dateFormatter.format(now));
+        
+        return "runs";
+    }
+    
+    @GetMapping("/groups")
+    public String groups(Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        
+        Account account = accRepo.findByUsername(username);
+        
+        User usr = usrRepo.findByAccount(account);
+            
+            
+        model.addAttribute("name",usr.getFirst_name());
+        
+        Date now = new Date();
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("EEEE, MMMM d, yyyy");
+        
+        model.addAttribute("date", dateFormatter.format(now));
+        
+        return "groups";
+    }
+}
